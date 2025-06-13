8662ff4c6831119e3296195f47c9d5ec0ebf6d7f
Delmat237 <142000059+Delmat237@users.noreply.github.com>
2025-06-13 11:06:06 +0100

    Update LoginResponse.java

--- src/main/java/com/rsm/dto/LoginResponse.java
+++ src/main/java/com/rsm/dto/LoginResponse.java
@@ -1,12 +1,16 @@
 package com.rsm.dto;
 
 import lombok.Data;
+import com.rsm.entity.Enseignant;
+
 @Data
 public class LoginResponse {
     private String token;
+    private Enseignant enseignant;
 
-    public LoginResponse(String token) {
+    public LoginResponse(String token,Enseignant enseignant) {
         this.token = token;
+        this.enseignant = enseignant;
     }
 }
 
